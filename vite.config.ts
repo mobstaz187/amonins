af809966ff34f7bed375c9f3c53ffa4d8f2b06cf
unknown <markmark092100@gmail.com>
2025-02-04 11:58:03 +0800

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: './',
   build: {
+    outDir: 'dist',
     assetsDir: 'assets',
     rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+      },
       output: {
         assetFileNames: (assetInfo) => {
-          if (assetInfo.name === 'f4d1593471d222ddebd973210265762a.woff2') {
-            return 'assets/[name][extname]';
+          const info = assetInfo.name.split('.')
+          const ext = info[info.length - 1]
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico|mp4/i.test(ext)) {
+            return `assets/${assetInfo.name}`
           }
-          return 'assets/[name]-[hash][extname]';
-        }
-      }
-    }
-  }
+          return `assets/[name]-[hash][extname]`
+        },
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+      },
+    },
+  },
+  publicDir: 'public',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
